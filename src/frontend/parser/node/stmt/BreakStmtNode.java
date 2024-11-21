c31c677da0ab975c834ac14dfa456d77745ee89d
真不想写代码 <3498314162@qq.com>
2024-11-21 21:08:45 +0800

    refactor(BreakStmtNode, ContinueStmtNode): Rewrite buildIR() method

--- src/frontend/parser/node/stmt/BreakStmtNode.java
+++ src/frontend/parser/node/stmt/BreakStmtNode.java
@@ -2,6 +2,7 @@ package frontend.parser.node.stmt;
 
 import frontend.ir.IRBuilder;
 import frontend.ir.llvm.ValueTable;
+import frontend.ir.llvm.value.BasicBlock;
 import frontend.ir.llvm.value.Value;
 import frontend.ir.llvm.value.instruction.terminator.Br;
 import frontend.semantic.SymbolTable;
@@ -24,8 +25,23 @@ public class BreakStmtNode extends StmtNode {
 
     @Override
     public Value buildIR() {
-        Br br = new Br.UnconditionalBr(IRBuilder.IR_BUILDER.getLocalVarName(), ValueTable.VALUE_TABLE.getLoopInfo().getLoopEnd());
+        //create a new basic block for break
+        BasicBlock breakBasicBlock = new BasicBlock(IRBuilder.IR_BUILDER.getBasicBlockName());
+        IRBuilder.IR_BUILDER.addBasicBlock(breakBasicBlock);
+
+        //add br to the current basic block
+        Br br = new Br.UnconditionalBr(IRBuilder.IR_BUILDER.getLocalVarName(), breakBasicBlock);
         IRBuilder.IR_BUILDER.addInstruction(br);
+
+        //break is the only instruction in its basic block
+        IRBuilder.IR_BUILDER.setCurrentBasicBlock(breakBasicBlock);
+        br = new Br.UnconditionalBr(IRBuilder.IR_BUILDER.getLocalVarName(), ValueTable.VALUE_TABLE.getLoopInfo().getLoopEnd());
+        IRBuilder.IR_BUILDER.addInstruction(br);
+
+        //create a new basic block for future instructions
+        BasicBlock newBasicBlock = new BasicBlock(IRBuilder.IR_BUILDER.getBasicBlockName());
+        IRBuilder.IR_BUILDER.addBasicBlock(newBasicBlock);
+        IRBuilder.IR_BUILDER.setCurrentBasicBlock(newBasicBlock);
 
         return null;
     }
