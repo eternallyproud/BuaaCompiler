25b0f416a0be2b8909a5952d708f16d3f68867dc
真不想写代码 <3498314162@qq.com>
2024-12-03 12:19:31 +0800

    feat: add remove redundant conversion operation optimization

--- src/optimize/ir/RemoveRedundantInstruction.java
+++ src/optimize/ir/RemoveRedundantInstruction.java
@@ -35,25 +35,52 @@ public class RemoveRedundantInstruction {
     }
 
     private void optimize(Function function) {
+        removeRedundantZext(function);
+        removeRedundantConversionOperation(function);
+    }
+
+    private void removeRedundantZext(Function function) {
         for (BasicBlock basicBlock : function.getBasicBlocks()) {
-            removeRedundantZext(basicBlock);
+            boolean hasChanged;
+            do {
+                ArrayList<Instruction> instructions = new ArrayList<>(basicBlock.getInstructions());
+                hasChanged = false;
+                for (Instruction instruction : instructions) {
+                    if (instruction instanceof ConversionOperation conversionOperation && conversionOperation.isZext()) {
+                        if (conversionOperation.getUsedValue(0) instanceof Constant constant) {
+                            conversionOperation.updateAllUsers(constant.convertTo(conversionOperation.getValueType()));
+                            conversionOperation.removeAllUse();
+                            basicBlock.removeInstruction(conversionOperation);
+                            hasChanged = true;
+                        }
+                    }
+                }
+            } while (hasChanged);
         }
     }
 
-    private void removeRedundantZext(BasicBlock basicBlock) {
-        boolean hasChanged;
-        do {
-            ArrayList<Instruction> instructions = new ArrayList<>(basicBlock.getInstructions());
-            hasChanged = false;
-            for (Instruction instruction : instructions) {
-                if (instruction instanceof ConversionOperation conversionOperation && conversionOperation.isZext()) {
-                    if (conversionOperation.getUsedValue(0) instanceof Constant constant) {
-                        conversionOperation.updateAllUsers(constant.convertTo(conversionOperation.getValueType()));
-                        basicBlock.removeInstruction(conversionOperation);
-                        hasChanged = true;
+    private void removeRedundantConversionOperation(Function function) {
+        for (BasicBlock basicBlock : function.getBasicBlocks()) {
+            boolean hasChanged;
+            do {
+                ArrayList<Instruction> instructions = new ArrayList<>(basicBlock.getInstructions());
+                hasChanged = false;
+                for (int i = 0; i < instructions.size() - 1; i++) {
+                    Instruction instruction1 = instructions.get(i);
+                    Instruction instruction2 = instructions.get(i + 1);
+                    if (instruction1 instanceof ConversionOperation conversionOperation1 && instruction2 instanceof ConversionOperation conversionOperation2) {
+                        if (conversionOperation1.getUserList().size() <= 1 && conversionOperation2.getUsedValue(0) == conversionOperation1 && conversionOperation1.getUsedValue(0).getValueType().equals(conversionOperation2.getValueType())) {
+                            conversionOperation2.updateAllUsers(conversionOperation1.getUsedValue(0));
+                            conversionOperation1.removeAllUse();
+                            conversionOperation2.removeAllUse();
+                            basicBlock.removeInstruction(conversionOperation1);
+                            basicBlock.removeInstruction(conversionOperation2);
+                            i++;
+                            hasChanged = true;
+                        }
                     }
                 }
-            }
-        } while (hasChanged);
+            } while (hasChanged);
+        }
     }
 }
